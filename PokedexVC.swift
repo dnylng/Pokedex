4d59448b71a10b81ed17ad64fa565849a1625d28
dnylng <iamdannyluong@gmail.com>
2017-07-06 18:28:14 -0400

    Implemented MVC skeleton, made pokemon model

---
+++ PokedexVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Danny Luong on 7/6/17.
+//  Copyright © 2017 dnylng. All rights reserved.
+//
+
+import UIKit
+
+class PokedexVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
