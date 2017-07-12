8b257da0f9cc33a286e6120744ecbbd6bb7b433a
dnylng <iamdannyluong@gmail.com>
2017-07-12 13:02:20 -0400

    Added func to back button and updated pokemon model
    The pokemon model now has type, defense, attack, and all the other detailed descriptors.

--- PokeDetailVC.swift
+++ PokeDetailVC.swift
@@ -12,11 +12,26 @@ class PokeDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     
+    @IBOutlet weak var mainImg: UIImageView!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var atkLbl: UILabel!
+    @IBOutlet weak var currEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         nameLbl.text = pokemon.name
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
 }
